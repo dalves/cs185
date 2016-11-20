ae255a22def6ac807ad2cc71f23585890ec4bfdc
David Alves <dalves@fb.com>
2016-11-20 10:27:33 -0800

    Updated dummy data to have movies be edges instead of nodes

--- dummy_data.py
+++ dummy_data.py
@@ -1,4 +1,3 @@
-movies = ['movie '+str(i) for i in range(1, 1000)]
 actors = ['actor '+str(i) for i in range(1, 1000)]
 
 from random import choice
@@ -6,7 +5,7 @@ from collections import Counter, defaultdict
 
 acted_in = defaultdict(set)
 for i in range(4000):
-    print(choice(actors) + ';' +choice(movies))
+    print(choice(actors) + ';' +choice(actors))
 
 
 
